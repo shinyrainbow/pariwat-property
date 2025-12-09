7cae0371ee1060f2eaafa4c0a7417a844f8a9b67
shinyrainbow <kornkanok.lieng@gmail.com>
2025-12-09 11:03:19 +0700

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -113,9 +113,8 @@ export default function RootLayout({
   return (
     <html lang="th">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="icon" href="/web-logo.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/web-logo.png" />
         <link rel="manifest" href="/manifest.json" />
       </head>
       <body className={`${kanit.variable} font-sans antialiased`}>
